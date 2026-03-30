c6367f5dd13b1ea3ff055e32221db6ee83ef50de
Vdabhi9328 <158403602+Vdabhi9328@users.noreply.github.com>
2026-03-30 08:47:05 +0000

    ch-4 compete

---
+++ Chapter_4_list/01.py
@@ -0,0 +1,18 @@
+print("hello")
+
+#---------Chapter 4 -------
+
+#------problem - 1 ---------
+fruits = []
+
+f1 = input("Enter fruit name ")
+fruits.append(f1)
+
+f2 = input("Enter fruit name ")
+fruits.append(f2)
+
+print(fruits)
+
+
+
+
---
+++ Chapter_4_list/03.py
@@ -0,0 +1,7 @@
+a = (34, 234, "Vimal")
+
+a[2] = "Vim"
+print(a)
+
+#a[2] = "Vim"
+#TypeError: 'tuple' object does not support item assignment
