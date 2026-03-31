53b29b720c1e23305bf8d0f3cf367eba8df09e45
Vdabhi9328 <158403602+Vdabhi9328@users.noreply.github.com>
2026-03-31 14:58:24 +0000

    dict complete

---
+++ Dict&sets/dict/02_dict_methos.py
@@ -0,0 +1,18 @@
+marks = {
+    "Vim" : 100,
+    "Mox" : 89,
+    "Dev" : 77,
+    0: "Vim"
+}
+
+
+# print(marks["Vim"])
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+
+# marks.update({"Vim" : 99, "Riya":89})
+# print(marks)
+
+print(marks.get("Vim"))
+print(marks["Vim"])
