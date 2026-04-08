d7d2ce31e74917e6fa2195f308832fc15ce21f95
Vdabhi9328 <158403602+Vdabhi9328@users.noreply.github.com>
2026-04-08 09:12:03 +0000

    chapter 7 done

---
+++ Chapter_7_loops/01_table.py
@@ -0,0 +1,12 @@
+n = int(input("Enter a number : "))
+
+for i in range(1,11):
+    print(f"{n} X {i} = {n*i}")
+print()
+# using while
+
+m = 1
+
+while(m<11):
+    print(f"{n} X {m} = {n*m}")
+    m+=1
---
+++ Chapter_7_loops/05_factorial.py
@@ -0,0 +1,9 @@
+# 5! = 5 X 4 X 3 X 2 X 1 
+
+n = int(input("Enter a number : "))
+
+fact = 1
+for i in range(1,n+1):
+    fact = fact*i
+
+print(f"The Factorial of {n} is {fact}")
